61b87b129bdf134fda29a44b26c6af06b4675a16
Adriel Venancio Bomfim <adrielvbomfim@gmail.com>
2020-03-19 01:19:44 -0300

    Implementação da barra de procura de marcadores

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,33 @@ class _MyAppState extends State<MyApp> {
 }
 
 //Tela de Buscar Item
-class AfterSplash extends StatelessWidget {
+class AfterSplash extends StatefulWidget {
+  @override
+  _AfterSplashState createState() => _AfterSplashState();
+}
+
+class _AfterSplashState extends State<AfterSplash> {
+  final TextEditingController _searchController = TextEditingController();
+  Icon _appbarIcon = Icon(Icons.search);
+  FocusNode _focusNode = FocusNode();
+
+  void _iconPressed() {
+    setState((){
+      if(_appbarIcon.icon == Icons.close) {
+        _appbarIcon = Icon(Icons.search);
+        WidgetsBinding.instance.addPostFrameCallback((_) => _searchController.clear());
+        FocusScope.of(context).requestFocus(new FocusNode());
+      }
+    });
+  }
+
+  void _barTapped(){
+    setState(() {
+      if (_appbarIcon.icon == Icons.search && !_focusNode.hasFocus)
+        _appbarIcon = Icon(Icons.close);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -42,17 +68,27 @@ class AfterSplash extends StatelessWidget {
                   border: Border.all(color: Colors.blueAccent, width: 4),
                   borderRadius: BorderRadius.circular(12)),
               child: Container(
+                margin: EdgeInsets.only(left: 10.0, bottom: 2.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text(
-                      "Insira o tipo de cancer ou marcador tumoral",
-                      style: TextStyle(fontSize: 15, color: Colors.black),
-                    ),
-                    Icon(
-                      Icons.search,
-                      color: Colors.black,
+                    Expanded(
+                      child: TextField(
+                        focusNode: _focusNode,
+                        onTap: () => _barTapped(),
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            suffixIcon: IconButton(
+                                splashColor: Colors.transparent,
+                                highlightColor: Colors.transparent,
+                                icon: _appbarIcon,
+                                onPressed: () => _iconPressed()
+                            ),
+                            hintText: 'Insira o tipo de cancer ou marcador tumoral'
+                        ),
+                      ),
                     ),
                   ],
                 ),

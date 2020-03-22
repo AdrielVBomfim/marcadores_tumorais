cbfca51d97d07e93c3be2a521d6beaa8083cef56
Adriel Venancio Bomfim <adrielvbomfim@gmail.com>
2020-03-22 00:50:10 -0300

    Finalização do filtro de busca

--- lib/pages/result_list_page.dart
+++ lib/pages/result_list_page.dart
@@ -3,78 +3,62 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:marcadorestumorais/database/model/encItemModel.dart';
 import 'package:marcadorestumorais/pages/result_page.dart';
-import '../database/dao/encItemDAO.dart';
 
 class ResultListPage extends StatelessWidget {
   bool flag;
-  String searchTerm;
+  List<EncItem> filteredList;
 
-  ResultListPage({this.flag = false, this.searchTerm});
-
-  List<EncItem> _filterItems(List<EncItem> itens, String searchTerm){
-
-  }
+  ResultListPage({this.flag = false, this.filteredList});
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    EncItemDao encItemDao = EncItemDao();
-    return FutureBuilder<List<EncItem>>(
-        future: encItemDao.getAllEncItems(),
-        builder: (BuildContext context, AsyncSnapshot<List<EncItem>> snapshot) {
-          if (!snapshot.hasData)
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          else{
-            print(searchTerm);
-            //List<EncItem> filteredList = _filterItems(snapshot.data, searchTerm);
-            return flag
-                ? Scaffold(
-                    appBar: AppBar(
-                      elevation: 0,
-                      backgroundColor: Colors.transparent,
-                    ),
-                    body: Container(
-                      child: ListView.separated(
-                          padding: const EdgeInsets.all(7),
-                          separatorBuilder: (BuildContext context, int index) =>
-                              const Divider(height: 4.0),
-                          itemCount: snapshot.data.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return InkWell(
-                              splashColor: Colors.cyan,
-                              onTap: () => Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => ResultPage(description: snapshot.data[index].description))),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.black12,
-                                ),
-                                height: 70,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: <Widget>[
-                                    //Image(image: AssetImage('assets/medicine.png')),
-                                    SizedBox(width: 10.0),
-                                    Flexible(
-                                      child: Text(
-                                        snapshot.data[index].title,
-                                        style: GoogleFonts.rubik(
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+    return flag
+        ? Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+            ),
+            body: Container(
+              child: ListView.separated(
+                  padding: const EdgeInsets.all(7),
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(height: 4.0),
+                  itemCount: filteredList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      splashColor: Colors.cyan,
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ResultPage(
+                                  description:
+                                      filteredList[index].description))),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black12,
+                        ),
+                        height: 70,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            //Image(image: AssetImage('assets/medicine.png')),
+                            SizedBox(width: 10.0),
+                            Flexible(
+                              child: Text(
+                                filteredList[index].title,
+                                style: GoogleFonts.rubik(
+                                  fontWeight: FontWeight.w500,
                                 ),
                               ),
-                            );
-                          }),
-                    ),
-                  )
-                : Container();
-          }
-        });
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          )
+        : Container();
   }
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:marcadorestumorais/database/model/encItemModel.dart';
 import 'package:marcadorestumorais/pages/result_list_page.dart';
+import '../database/dao/encItemDAO.dart';
 
 //Tela de Buscar Item
 class SearchPage extends StatefulWidget {
@@ -11,9 +13,11 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   final TextEditingController _searchController = TextEditingController();
+  List<EncItem> filteredList;
   Icon _appbarIcon = Icon(Icons.search);
   FocusNode _focusNode = FocusNode();
   bool _flag = false;
+  EncItemDao encItemDao = EncItemDao();
 
   void initState() {
     super.initState();
@@ -46,64 +50,81 @@ class _SearchPageState extends State<SearchPage> {
     });
   }
 
+  List<EncItem> _filterItems(List<EncItem> itens, String searchTerm) {
+    List<EncItem> newList = List<EncItem>();
+
+    itens.forEach((item) {
+      if (item.title.contains(searchTerm)) newList.add(item);
+    });
+
+    return newList;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(48.0),
-          child: SafeArea(
-            child: Container(
-              margin: EdgeInsets.all(7.0),
-              width: MediaQuery.of(context).size.width,
-              height: 48,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.brown, width: 1),
-                    borderRadius: BorderRadius.circular(12)),
-                child: Container(
-                  margin: EdgeInsets.only(left: 10.0, bottom: 2.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          focusNode: _focusNode,
-                          //onTap: () => _barTapped(),
-                          textInputAction: TextInputAction.search,
-                          onSubmitted: (String value) async {
-                            _flag = true;
-                          },
-                          controller: _searchController,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            suffixIcon: IconButton(
-                                color: Colors.brown,
-                                splashColor: Colors.transparent,
-                                highlightColor: Colors.transparent,
-                                icon: _appbarIcon,
-                                onPressed: () => _iconPressed()),
-                            hintText: 'Digite aqui para fazer uma busca',
-                            hintStyle: GoogleFonts.rubik(
-                              fontWeight: FontWeight.w500,
-                            ),
+    return FutureBuilder<List<EncItem>>(
+        future: encItemDao.getAllEncItems(),
+        builder: (BuildContext context, AsyncSnapshot<List<EncItem>> snapshot) {
+          if (!snapshot.hasData)
+            filteredList = List<EncItem>();
+            return Scaffold(
+              extendBodyBehindAppBar: true,
+              appBar: AppBar(
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(48.0),
+                  child: SafeArea(
+                    child: Container(
+                      margin: EdgeInsets.all(7.0),
+                      width: MediaQuery.of(context).size.width,
+                      height: 48,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.brown, width: 1),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Container(
+                          margin: EdgeInsets.only(left: 10.0, bottom: 2.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  focusNode: _focusNode,
+                                  //onTap: () => _barTapped(),
+                                  textInputAction: TextInputAction.search,
+                                  onSubmitted: (String value) async {
+                                    _flag = true;
+                                    filteredList = _filterItems(snapshot.data, _searchController.text);
+                                  },
+                                  controller: _searchController,
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    suffixIcon: IconButton(
+                                        color: Colors.brown,
+                                        splashColor: Colors.transparent,
+                                        highlightColor: Colors.transparent,
+                                        icon: _appbarIcon,
+                                        onPressed: () => _iconPressed()),
+                                    hintText: 'Digite aqui para fazer uma busca',
+                                    hintStyle: GoogleFonts.rubik(
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
-        ),
-      ),
-      body: ResultListPage(flag: _flag, searchTerm: _searchController.text),
-    );
+              body: ResultListPage(flag: _flag, filteredList: filteredList),
+            );
+        });
   }
 }
